d888c50465223ae1103f16d64a46fcc68aae6c40
Victor <victorn99@gmail.com>
2014-05-18 11:30:52 -0300

    cliente

--- obligatorio1-arqui-livianas/src/uy/edu/ort/MainUserService.java
+++ obligatorio1-arqui-livianas/src/uy/edu/ort/MainUserService.java
@@ -1,9 +1,12 @@
 package uy.edu.ort;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MainUserService {
 
     public static void main(String[] args) {
-//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("resources/application-context.xml");
+       ApplicationContext applicationContext = new ClassPathXmlApplicationContext("resources/application-context.xml");
 //        UserService userService = (UserService) applicationContext.getBean("userService");
 //
 //        List<User> users = userService.listUsers();

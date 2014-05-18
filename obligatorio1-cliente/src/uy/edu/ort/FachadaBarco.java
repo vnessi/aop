412813263842e8ed734bada828e88849161aeadf
Victor <victorn99@gmail.com>
2014-05-18 16:28:31 -0300

    Metodos Barco

---
+++ obligatorio1-cliente/src/uy/edu/ort/FachadaBarco.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uy.edu.ort;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import uy.edu.ort.model.Barco;
+import uy.edu.ort.service.BarcoService;
+import uy.edu.ort.service.BussinesException;
+
+/**
+ *
+ * @author victor
+ */
+public class FachadaBarco {
+    
+    private static final ApplicationContext ctx = new ClassPathXmlApplicationContext("resources/application-context.xml");
+    private static final BarcoService barcoDao = (BarcoService) ctx.getBean("barcoService");
+    
+    public static void agregarBarco(String argumentos) {
+        try {
+            
+            String[] args = argumentos.split(",");
+            
+            Barco b = new Barco();
+            b.setCodigo(args[0]);
+            b.setNombre(args[1]);
+            b.setBandera(args[2]);
+            b.setCapacidadTransporte(Integer.valueOf(args[3]));
+            b.setAnioFabricacion(Integer.valueOf(args[4]));
+            b.setCantidadTripulantes(Integer.valueOf(args[5]));
+            barcoDao.addBarco(b);
+            System.out.println(">> Barco agregado con exito");
+        } catch (BussinesException ex) {
+            Logger.getLogger(FachadaBarco.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void eliminarBarco(String codigo) {
+        try {
+            barcoDao.removeBarco(codigo);
+            System.out.println(">> Barco eliminado con exito");
+        } catch (BussinesException ex) {
+            Logger.getLogger(FachadaBarco.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void modificarBarco(String codigo, String argumentos) {
+        try {
+            Barco b = barcoDao.obtenerBarco(codigo);
+            
+            String[] args = argumentos.split(",");
+            
+            b.setCodigo(args[0]);
+            b.setNombre(args[1]);
+            b.setBandera(args[2]);
+            b.setCapacidadTransporte(Integer.valueOf(args[3]));
+            b.setAnioFabricacion(Integer.valueOf(args[4]));
+            b.setCantidadTripulantes(Integer.valueOf(args[5]));
+            barcoDao.modifyBarco(b);
+            System.out.println(">> Barco modificado con exito");
+        } catch (BussinesException ex) {
+            Logger.getLogger(FachadaBarco.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    public static void mostrarBarco(String codigo) {
+        try {
+            Barco barco = barcoDao.obtenerBarco(codigo);
+            System.out.println("\t\tCodigo \t\tNombre \t\tBandera \t\tCapacidad(kgs) \t\tAño \t\tCantidadTripulantes");
+            System.out.println("\t\t" + barco.getCodigo() +
+                    " \t\t" + barco.getNombre() + " \t\t" + barco.getBandera() +
+                    " \t\t" + barco.getCapacidadTransporte() + " \t\t"+ String.valueOf(barco.getAnioFabricacion()) +
+                    " \t\t" + barco.getCantidadTripulantes());
+        } catch (BussinesException ex) {
+            Logger.getLogger(FachadaBarco.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            
+    }
+    
+    public static void listarBarcos() {
+        try {
+            List<Barco> barcos = barcoDao.listBarcos();
+            System.out.println("\t\tCodigo \t\tNombre \t\tBandera \t\tCapacidad(kgs) \t\tAño \t\tCantidadTripulantes");
+            for (Barco barco : barcos) {
+                System.out.println("\t\t" + barco.getCodigo() + 
+                                   " \t\t" + barco.getNombre() + " \t\t" + barco.getBandera() +
+                                   " \t\t" + barco.getCapacidadTransporte() + " \t\t"+ String.valueOf(barco.getAnioFabricacion()) +
+                                   " \t\t" + barco.getCantidadTripulantes());
+            }
+        } catch (BussinesException ex) {
+            Logger.getLogger(FachadaBarco.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
--- obligatorio1-cliente/src/uy/edu/ort/MainCliente.java
+++ obligatorio1-cliente/src/uy/edu/ort/MainCliente.java
@@ -5,11 +5,9 @@
  */
 package uy.edu.ort;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import uy.edu.ort.model.Barco;
-import uy.edu.ort.service.BarcoService;
-import uy.edu.ort.service.BussinesException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  *
@@ -20,40 +18,198 @@ public class MainCliente {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        try {
-            ApplicationContext applicationContext = new ClassPathXmlApplicationContext("resources/application-context.xml");
-            
-//        UserService userService = (BarcoService) applicationContext.getBean("userService");
-//        List<Barco> users = userService.listUsers();
-//        if (users != null) {
-//            for (User user : users) {
-//                System.out.println(user);
-//            }
+    public static void main(String[] args) throws IOException {
+//        try {
+//            ApplicationContext applicationContext = new ClassPathXmlApplicationContext("resources/application-context.xml");
+//            
+//            ArriboService aDao = (ArriboService) applicationContext.getBean("arriboService");
+//            
+//            BarcoService barcoDao = (BarcoService) applicationContext.getBean("barcoService");
+//            Barco b = new Barco();
+//            b.setAnioFabricacion(2);
+//            b.setBandera("si tiene");
+//            b.setCantidadTripulantes(22);
+//            b.setCapacidadTransporte(1000);
+//            b.setCodigo("no code");
+//            b.setNombre("name barcoooooooo");
+//            barcoDao.addBarco(b);
+//            
+//            //ContenedorService cDao = (ContenedorService) applicationContext.getBean("contenedorService");
+//            Contenedor c = new Contenedor();
+//            c.setCapacidad(10);
+//            c.setCodigo("1");
+//            c.setMarca("Marqueta");
+//            c.setModelo("Modelo");
+//            //cDao.addContenedor(c);
+//            //cDao.addContenedor(c);
+//            
+//            //b.setBandera("Chile");
+//            //barcoDao.modifyBarco(b);
+//            
+//            
+//            List<Contenedor> cList = new ArrayList<Contenedor>();
+//            cList.add(c);
+//            aDao.registrarArribo(b, cList, "Descripcion", "Desconocido");
+//            
+//            
+//            
+//        } catch (BussinesException ex) {
+//            //Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
 //        }
-
-//        User u = new User("pp", "Piter");
-//        userService.addUser(u);
+//        ApplicationContext ctx = new ClassPathXmlApplicationContext("resources/application-context.xml");
+//        ApplicationContext ctx1 = new ClassPathXmlApplicationContext("resources/application-context.xml");
+//        ArriboService aservice = (ArriboService) ctx1.getBean("arriboService");
+//
+//        BarcoService barcoDao = (BarcoService) ctx.getBean("barcoService");
+//        //BarcoDao barcoDaoo = (BarcoDao) ctx.getBean("barcoDao");
+//        Barco b = new Barco();
+//        b.setAnioFabricacion(19);
+//        b.setBandera("no tiene lalala");
+//        b.setCantidadTripulantes(22);
+//        b.setCapacidadTransporte(1000);
+//        b.setCodigo("no code");
+//        b.setNombre("name barc");
 //        
-//        users = userService.listUsers("Peter");
-//        if (users != null) {
-//            for (User user : users) {
-//                System.out.println(user);
-//            }
+//        try {        
+//        
+//            barcoDao.addBarco(b);
+//            //System.out.println(barcoDao.listBarcos().toString());
+//            
+//            //b.setBandera("Luna");
+//            //barcoDao.modifyBarco(b);
+//            
+//            List<Contenedor> conts = new ArrayList<>();
+//            Contenedor c = new Contenedor();
+//            c.setCapacidad(33);
+//            c.setCodigo("CODE cont");
+//            c.setMarca("Marca");
+//            c.setModelo("nice1");
+//            
+//            conts.add(c);
+//            aservice.registrarArribo(b, conts, "Descriptioin ", "CHINA");
+
+            //System.out.println(aservice.generarReporteArribosMes(5));
+        //aservice.registrarArribo(b, null, null, null);
+        //  BarcoDao bdao ;
+        //System.out.print(bdao.obtenerTodos());*/
+//        } catch (BussinesException ex) {
+//            //Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
 //        }
-            
-            BarcoService barcoDao = (BarcoService) applicationContext.getBean("barcoService");
-            Barco b = new Barco();
-            b.setAnioFabricacion(1990);
-            b.setBandera("si tiene");
-            b.setCantidadTripulantes(22);
-            b.setCapacidadTransporte(1000);
-            b.setCodigo("no code");
-            b.setNombre("name barc");
-            barcoDao.addBarco(b);
-        } catch (BussinesException ex) {
-            //Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//        Runtime.getRuntime().exec("cls");    
+//        printHeader();
+//            printCommands();
+        boolean noSalir = true;
+        String arg;
+        while (noSalir) {
+            printHeader();
+            printCommands();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            String line = bufferedReader.readLine();
+
+            String[] read = line.trim().split(" ");
+            String comando = read[0];
+
+            String mensaje;
+            switch (comando) {
+                case "1":
+                    System.out.println(">>Agregar Barco: "
+                            + "\nIngrese los datos del barco separados por coma "
+                            + "(Codigo,Nombre,Bandera,Capacidad(kgs),Año,Cantidad Tripulantes)\n");
+                    System.out.print(">> ");
+                    arg = bufferedReader.readLine();
+                    try {
+                        if (checkArgs(arg)) {
+                            FachadaBarco.agregarBarco(arg);
+                        }
+                    } catch (Exception ex) {
+                        System.out.println("Lo siento a ocurrido un error, Error: " + ex.getMessage());
+                    }
+                    break;
+                case "2":
+                    System.out.println(">>Eliminar Barco: \n");
+                    FachadaBarco.listarBarcos();
+                    System.out.println(">>Ingrese el codigo del barco que desea eliminar: \n");
+                    System.out.print(">> ");
+                    arg = bufferedReader.readLine();
+                    try {
+                        if (checkArgs(arg)) {
+                            FachadaBarco.eliminarBarco(arg);
+                        }
+                    } catch (Exception ex) {
+                        System.out.println("Lo siento a ocurrido un error, Error: " + ex.getMessage());
+                    }
+                    break;
+
+                case "3":
+                    System.out.println(">>Modificar Barco: \n");
+                    FachadaBarco.listarBarcos();
+                    System.out.println(">>Ingrese el codigo del barco que desea modificar: \n");
+                    System.out.print(">> ");
+                    String codBarco = bufferedReader.readLine();
+                    try {
+                        if (checkArgs(codBarco)) {
+                            FachadaBarco.mostrarBarco(codBarco);
+                            System.out.println("\nIngrese los datos del barco separados por coma "
+                            + "(Codigo,Nombre,Bandera,Capacidad(kgs),Año,Cantidad Tripulantes)\n");
+                            System.out.print(">> ");
+                            arg = bufferedReader.readLine();
+                            if (checkArgs(arg)) {
+                                FachadaBarco.modificarBarco(codBarco, arg);
+                            }
+                        }
+                    } catch (Exception ex) {
+                        System.out.println("Lo siento a ocurrido un error, Error: " + ex.getMessage());
+                    }
+                    break;
+                    
+                case "4":
+                    System.out.println(">>Listar Barcos\n");
+                    try {
+                        FachadaBarco.listarBarcos();
+                    } catch (Exception ex) {
+                        System.out.println("Lo siento a ocurrido un error, Error: " + ex.getMessage());
+                    }
+                    break;
+
+                case "exit":
+                    System.out.println(">>Salir");
+                    noSalir = false;
+                    break;
+                default:
+                    System.out.println("No existe comando!");
+                    printCommands();
+                    break;
+            }
         }
+        System.exit(0);
+    }
+
+    private static void printHeader() {
+        System.out.println("=====================================================================");
+        System.out.println("=====================================================================");
+        System.out.println("==============Arquitecturas de Software Livianas con Java==============");
+        System.out.println("==============================Obligatorio 1==========================");
+        System.out.println("=====================================================================");
+        System.out.println("================ Bruno Montaner ========= Victor Nessi ==============\n");
+    }
+
+    private static void printCommands() {
+        System.out.println("Ingrese la opcion deseada");
+        System.out.println("Comandos:");
+        System.out.println(">> comando -\tDescripcion ");
+        System.out.println(">> 1 -\tAgregar Barco");
+        System.out.println(">> 2 -\tEliminar Barco");
+        System.out.println(">> 3 -\tModificar Barco");
+        System.out.println(">> 4 -\tListar Barcos");
+    }
+
+    private static Boolean checkArgs(String s) {
+        if (s.isEmpty()) {
+            System.out.println("Debe ingresar un argumento luego del comando");
+            return false;
+        }
+        return true;
+
     }
 
 }
